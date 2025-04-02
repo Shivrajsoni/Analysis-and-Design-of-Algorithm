0e797ab2f07399ce45ec95c9579c357dc64fd5f4
Shivraj Soni <shivrajsoni09022005@gmail.com>
2025-04-02 07:19:06 +0530

    added prims and kruskal algortihm

--- minimum_spanning_tree/prims.cpp
+++ minimum_spanning_tree/prims.cpp
@@ -1,63 +1,98 @@
-#include<bits/stdc++.h>
-#include<queue>
-
+// A C++ program for Prim's Minimum
+// Spanning Tree (MST) algorithm. The program is
+// for adjacency matrix representation of the graph
+#include <bits/stdc++.h>
 using namespace std;
-class Solution {
-  public:
-  int spanning_tree(int V, vector<vector<int,int>>adj[]){
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-    vector<int>vis(V,0);
-    int sum = 0;
-    // wt,node
-    pq.push({0,0});
 
-    while (!pq.empty())
-    {
-      auto it = pq.top();
-      pq.pop();
+// A utility function to find the vertex with
+// minimum key value, from the set of vertices
+// not yet included in MST
+int minKey(vector<int> &key, vector<bool> &mstSet) {
 
-      int node = it.second;
-      int weight = it.first;
+  // Initialize min value
+  int min = INT_MAX, min_index;
 
-      if(vis[node]==1)
-      continue;
+  for (int v = 0; v < mstSet.size(); v++)
+    if (mstSet[v] == false && key[v] < min)
+      min = key[v], min_index = v;
 
-      vis[node]=1;
-      sum+=weight;
-      for(auto it: adj[node]){
-        int adjNode = it[0];
-        int edW = it[1];
-        
-        if(!vis[adjNode]){
-          pq.push({edW,adjNode});
-        }
+  return min_index;
+}
 
-      }
-    }  
-    return sum;
+// A utility function to print the
+// constructed MST stored in parent[]
+void printMST(vector<int> &parent, vector<vector<int>> &graph) {
+  cout << "Edge \tWeight\n";
+  for (int i = 1; i < graph.size(); i++)
+    cout << parent[i] << " - " << i << " \t" << graph[parent[i]][i] << " \n";
+}
+
+// Function to construct and print MST for
+// a graph represented using adjacency
+// matrix representation
+void primMST(vector<vector<int>> &graph) {
+
+  int V = graph.size();
+
+  // Array to store constructed MST
+  vector<int> parent(V);
+
+  // Key values used to pick minimum weight edge in cut
+  vector<int> key(V);
+
+  // To represent set of vertices included in MST
+  vector<bool> mstSet(V);
+
+  // Initialize all keys as INFINITE
+  for (int i = 0; i < V; i++)
+    key[i] = INT_MAX, mstSet[i] = false;
+
+  // Always include first 1st vertex in MST.
+  // Make key 0 so that this vertex is picked as first
+  // vertex.
+  key[0] = 0;
+
+  // First node is always root of MST
+  parent[0] = -1;
+
+  // The MST will have V vertices
+  for (int count = 0; count < V - 1; count++) {
+
+    // Pick the minimum key vertex from the
+    // set of vertices not yet included in MST
+    int u = minKey(key, mstSet);
+
+    // Add the picked vertex to the MST Set
+    mstSet[u] = true;
+
+    // Update key value and parent index of
+    // the adjacent vertices of the picked vertex.
+    // Consider only those vertices which are not
+    // yet included in MST
+    for (int v = 0; v < V; v++)
+
+      // graph[u][v] is non zero only for adjacent
+      // vertices of m mstSet[v] is false for vertices
+      // not yet included in MST Update the key only
+      // if graph[u][v] is smaller than key[v]
+      if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
+        parent[v] = u, key[v] = graph[u][v];
   }
 
-};
+  // Print the constructed MST
+  printMST(parent, graph);
+}
 
+// Driver's code
+int main() {
+  vector<vector<int>> graph = {{0, 2, 0, 6, 0},
+                               {2, 0, 3, 8, 5},
+                               {0, 3, 0, 0, 7},
+                               {6, 8, 0, 0, 9},
+                               {0, 5, 7, 9, 0}};
 
-int main(){
-    int V = 3, E = 3, S = 2;
-    vector<vector<int>> adj[V];
-    vector<vector<int>> edges;
-    vector<int> v1{1, 1}, v2{2, 6}, v3{2, 3}, v4{0, 1}, v5{1, 3}, v6{0, 6};
-    int i = 0;
-    adj[0].push_back(v1);
-    adj[0].push_back(v2);
-    adj[1].push_back(v3);
-    adj[1].push_back(v4);
-    adj[2].push_back(v5);
-    adj[2].push_back(v6);
-  
-  Solution obj;
-
-  int ans = obj.spanning_tree(V,adj);
-
-  cout<<"The sum of minimum Spanning tree is : "<<ans<<endl;
+  // Print the solution
+  primMST(graph);
 
   return 0;
 }
